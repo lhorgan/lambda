c8a65f3bb9aa1a6934a55ec5c1699cd9b6c7dabe
Luke Horgan <horgan.l@husky.neu.edu>
2020-03-04 18:51:43 -0500

    well, everything

--- l.js
+++ l.js
@@ -3,42 +3,162 @@ var AWS = require('aws-sdk');
 // Set the region 
 AWS.config.update({region: 'us-east-1'});
 var fs = require('fs');
-
-process.env.UV_THREADPOOL_SIZE = 128;
+const lineByLine = require('n-readlines');
+var level = require('level');
 
 var lambda = new AWS.Lambda();
 
-function collectIPs(region, count, start) {
-    AWS.config.update({region: region});
-    let ips = [];
-    let numProcessed = 0;
+class Earl {
+    constructor(ifname, ofname, dbname) {
+        this.lambda_names = [];
+        this.ips = {};
+        this.readstream = new lineByLine(ifname);
+        this.db = level(dbname);
+        this.allLinesRead = false;
+        this.urlsToWrite = [];
 
-    for(let i = 0; i < count; i++) {
-        var params = {
-            FunctionName: "hydrate-" + (start + i), /* required */
-            Payload: JSON.stringify({"ip": true})
-        };
-
-        lambda.invoke(params, function(err, data) {
-            numProcessed++;
-            if(err) {
-                console.log("Something went wrong");
-            } // an error occurred
-            else {
-                console.log(data);
-                let ip = JSON.parse(data["Payload"])["ip"];
-                console.log(ip + ": " + numProcessed);
-                ips.push(ip);
-            }
-
-            if(numProcessed === count) {
-                console.log("All IPs have been processed");
-                let uniqueIPs = new Set(ips);
-                console.log(uniqueIPs.size);
-            }
+        this.collectIPs(() => {
+            setTimeout(() => {
+                console.log("Collecting ips");
+                this.collectIPs();
+            }, 3*60*1000);
         });
+
+        setInterval(async () => {
+            await this.expand();
+        }, 1000);
     }
-    console.log("\n\nALL INVOCATIONS HAVE BEEN MADE\n\n");
+
+    async expand() {
+        let ipFreeze = JSON.parse(JSON.stringify(this.ips));
+
+        for(ip in ipFreeze) {
+            let url = await this.getNextURL();
+            let name = ipFreeze[ip][0];
+            let region = ipFreeze[ip][1];
+
+            AWS.config.update({region: region});
+
+            var params = {
+                FunctionName: name,
+                Payload: JSON.stringify({"url": url})
+            };
+
+            lambda.invoke(params, (err, data) => {
+                let message = {};
+                if(err) {
+                    message["orig_url"] = url;
+                    message["err"] = true;
+                    message["msg"] = err.toString();
+                    message["url"] = null;
+                    message["time"] = null;
+                }
+                else {
+                    let res = JSON.parse(data["Payload"]);
+                    message.err = res.error;
+                    message.orig_url = res.orig_url;
+                    message.url = url;
+                    message.time = res.diff;
+                    message.msg = res.message;
+                }
+                this.urlsToWrite.push(message);
+                if(this.urlsToWrite.length >= 50) {
+                    this.writeURLs();
+                }
+            });
+        }
+    }
+
+    writeURLs() {
+        //console.log("Writing a batch of URLs");
+        let urlsCopy = JSON.parse(JSON.stringify(this.urlsToWrite)); // live with it
+        this.urlsToWrite = [];
+        let urlStr = "";
+        for(let i = 0; i < urlsCopy.length; i++) {
+            urlStr += urlsCopy[i].url + "\t" + 
+                      urlsCopy[i].orig_url + "\t" + 
+                      urlsCopy[i].time + "\t" +
+                      urlsCopy[i].err + "\t" +
+                      urlsCopy[i].msg;
+            urlStr += "\r\n";
+
+            this.db.put(urlsCopy[i].origURL, "", () => {});
+        }
+
+        var stream = fs.createWriteStream(this.results_name, {flags:'a'});
+        stream.write(urlStr);
+        stream.end();
+    }
+
+    async getNextURL() {
+        while(true) { // this'll just keep going 'til we find something that isn't in the database
+            let line = this.readstream.next();
+
+            if(line) {
+                this.urlCount++;
+                line =  line.toString("utf-8");
+                let [url, year] = line.trim().split("\t");
+
+                let inDB = await this.db.get(url)
+                                .then((value) => { return true })
+                                .catch((err) => { return false });
+                
+                if(!inDB) {
+                    if(!year) {
+                        year = 2020;
+                    }
+                    return [url, year];
+                }
+                else {
+                    console.log("We already processed " + url);
+                }
+            }
+            else {
+                if(this.allLinesRead === false) {
+                    console.log("End of file reached!");
+                }
+                this.allLinesRead = true;
+                return [null, null];
+            }
+        }
+    }
+
+    collectIPs(cb) {
+        let ips = {};
+        let numProcessed = 0;
+    
+        for(let i = 0; i < this.lambda_names.length; i++) {
+            let name = this.lambda_names[i][0];
+            let region = this.lambda_names[i][1];
+
+            AWS.config.update({region: region});
+
+            var params = {
+                FunctionName: name,
+                Payload: JSON.stringify({"ip": true})
+            };
+    
+            lambda.invoke(params, (err, data) => {
+                numProcessed++;
+                if(err) {
+                    console.log("Something went wrong");
+                } // an error occurred
+                else {
+                    console.log(data);
+                    let ip = (JSON.parse(data["Payload"])["ip"]).trim();
+                    //console.log(ip + ": " + numProcessed);
+                    //ips.push(ip);
+                    ips[ip] = [name, region];
+                }
+    
+                if(numProcessed === count) {
+                    console.log("All IPs have been processed");
+                    this.ips = ips;
+                    cb();
+                }
+            });
+        }
+    }
 }
 
 /* This example creates a Lambda function. */
@@ -67,7 +187,7 @@ function launch(count, numLaunched, region, update) {
             f = lambda.updateFunctionConfiguration;
         }
 
-        f(params, function(err, data) {
+        f(params, (err, data) => {
             if(err) {
                 console.log(err);
                 setTimeout(() => { launch(count, numLaunched, region); }, 1000);
@@ -91,7 +211,7 @@ function update(count, numUpdated, region) {
             FunctionName: "hydrate-" + numUpdated, 
         };
         
-        lambda.updateFunctionCode(params, function(err, data) {
+        lambda.updateFunctionCode(params, (err, data) => {
             if(err) {
                 console.log(err);
                 setTimeout(() => { update(count, numUpdated, region); }, 1000);
@@ -106,4 +226,4 @@ function update(count, numUpdated, region) {
 
 //update(900, 0, "us-east-1");
 //launch(900, 501, "us-east-1");
-collectIPs("us-east-1", 150, 0);
+//collectIPs("us-east-1", 900, 0);
